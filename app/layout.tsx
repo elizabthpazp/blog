5be8ed7382b3d184f5272dc8d01254d96a888112
elizabthpazp <elizabthpazp695@gmail.com>
2023-10-19 17:12:19 -0400

    yy

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "../styles/globals.css";
 
 let title = "Learn about Web Development with me";
 let description = "It doesn't matter if you are just starting out or you are an expert, there is always something new that you can learn and I would really like to be able to share it with you.";
-let ogimage = "./fav.png";
-let sitename = "elizabthpazp.io";
+let ogimage = "./logo.png";
+let sitename = "elizabthpazp";
  
 export const metadata: Metadata = {
   title,
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     images: [ogimage],
     title,
     description,
-    url: "https://elizabthpazp.io",
+    url: "https://blog-elizabthpazp.vercel.app/",
     siteName: sitename,
     locale: "en_US",
     type: "website",

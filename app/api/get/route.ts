42bbc5827a038a9c6974785ad44458001b2aa568
elizabthpazp <elizabthpazp695@gmail.com>
2023-11-05 11:11:29 -0500

    tests

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -2,13 +2,16 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
+  console.log('entra')
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
   try {
     const result =
       await sql`SELECT count FROM posts WHERE id = ${id}`;
+      console.log(result)
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error }, { status: 500 });
   }
 }

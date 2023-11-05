a3094eb260b8d6b416967f943a71240ce20ceef3
elizabthpazp <elizabthpazp695@gmail.com>
2023-11-05 11:20:20 -0500

    fixes supuestos

--- app/[lang]/[slug]/page.tsx
+++ app/[lang]/[slug]/page.tsx
@@ -199,7 +199,7 @@ export default async function Learn({
     <PostPreview key={post.slug} {...post} />
   ));
 
-  const res = await fetch(`https://blog-elizabthpazp.vercel.app/api/get?id=${slug}`, { cache: "no-cache" })
+  const res = await fetch(`https://blog-elizabthpazp.vercel.app/api/get?id=${slug}`)
   const data = await res.json()  
    
   return (
@@ -213,9 +213,9 @@ export default async function Learn({
         title={dictionary.search} 
       />
 
-      {/* <div className="float-right row-auto mr-6 likeCounter"> 
+      <div className="float-right row-auto mr-6 likeCounter"> 
         <LikeCount count={data?.result?.rows[data?.result?.rows?.length-1]?.count} title={slug}></LikeCount>
-      </div> */}
+      </div>
 
       <main className="w-full items-center justify-center px-4 sm:mt-9 mt-9 background-gradient">
         <Markdown
--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -1,8 +1,7 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
-export async function GET(request: Request) {
- 
+export async function GET(request: Request) { 
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
   try {
@@ -10,8 +9,7 @@ export async function GET(request: Request) {
       await sql`SELECT count FROM posts WHERE id = ${id}`;
       
     return NextResponse.json({ result }, { status: 200 });
-  } catch (error) {
-    
+  } catch (error) { 
     return NextResponse.json({ error }, { status: 500 });
   }
 }

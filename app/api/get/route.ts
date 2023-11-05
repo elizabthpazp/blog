1c7bd18eee2ef02d214dcf6175a58d624eadd07e
elizabthpazp <elizabthpazp695@gmail.com>
2023-11-05 12:02:38 -0500

    fixes

--- app/api/get/route.ts
+++ app/api/get/route.ts
@@ -4,11 +4,13 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) { 
   const { searchParams } = new URL(request.url);
   const id = searchParams.get('id');
- 
+  try {
     const result =
       await sql`SELECT count FROM posts WHERE id = ${id}`;
       
     return NextResponse.json({ result }, { status: 200 });
-   
+  } catch (error) { 
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
 

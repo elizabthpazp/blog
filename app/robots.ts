3e787666bc3e81eabf83f575561bb32ca7e72dd5
elizabthpazp <elizabthpazp695@gmail.com>
2023-11-10 15:34:52 -0500

    robtss

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '',
+    },
+    sitemap: 'https://blog-elizabthpazp.vercel.app/sitemap.xml',
+  }
+} 

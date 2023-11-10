8f4c70542673fbd7e0ba236e3fac6b63168d1174
elizabthpazp <elizabthpazp695@gmail.com>
2023-11-10 15:22:14 -0500

    impfoves

--- components/LikeCount.tsx
+++ components/LikeCount.tsx
@@ -13,26 +13,29 @@ export default function LikeCount({ count, title, animation }: {count: any, titl
   let insertLike=async() =>{ 
     setCount(count2++) 
       const res = await fetch(`https://blog-elizabthpazp.vercel.app/api/post?id=${title}&count=${count2}`, {
-        method: 'POST', next: { tags: [title, count2] }, headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        } })
+        cache: 'force-cache' || 'no-cache',
+        method: 'POST',
+        mode: "cors",
+        headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      } })
       const data = await res.json() 
       console.log(data) 
   }
   
   return ( 
-      <button className={animation?"flex cursor-pointer pt-2 pr-2 pl-2 border light:border-gray-300 dark:border-gray-800 hover:border-violet-500 shadow-xl border-2": "flex cursor-pointer pt-2 pr-2 pl-2"} onClick={() => insertLike()} style={{borderRadius:'20px'}} name="like" title="like"> 
+      <button className={animation?"flex cursor-pointer absolute mr-btn pt-2 pr-2 pl-2 border light:border-gray-300 dark:border-gray-800 hover:border-violet-500 shadow-xl border-2": "flex cursor-pointer pt-2 pr-2 pl-2"} onClick={() => insertLike()} style={{borderRadius:'20px'}} name="like" title="like"> 
       <img
           alt="like"
           title="like"
           src={links.iconLike}
-          className={animation?'forcedImage cursor-pointer pt-1 click:animate-ping animate-bounce':"forcedImage cursor-pointer pb-1 click:animate-ping"}
+          className={animation?'forcedImage relative pt-1 click:animate-ping animate-bounce':"forcedImage pb-1"}
           width={30}
           height={30} 
         /> 
-        <p className="ml-2 cursor-pointer">{count2}</p>
+        <p className="ml-2 relative">{count2}</p>
       </button>
     )
 }
--- components/LikesList.tsx
+++ components/LikesList.tsx
@@ -1,24 +1,29 @@
 "use client"  
 
 const getLikesPage = async (slug: any) => {
-  const res = await fetch(`https://blog-elizabthpazp.vercel.app/api/get?id=${slug}`, {headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  }, cache: 'force-cache' || 'no-store'})
+  const res = await fetch(`https://blog-elizabthpazp.vercel.app/api/get?id=${slug}`, {
+    cache: 'force-cache' || 'no-cache',
+    method: "GET",
+    mode: "cors",
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    } })
   const data = await res.json() 
  
   return data?.result?.rows[data?.result?.rows?.length-1]?.count;
 };
 
-export default async function LikeCount({ slug }: {slug: any}): Promise<JSX.Element>
+export default async function LikeCount({ slug }: {slug: any})
 {   
   const likes = await getLikesPage(slug);
+  if(likes!=null && likes!=undefined)
   return ( 
     <div className="float-right" style={{marginTop: "-45px", zoom: "70%"}}> 
-    {/* @ts-expect-error */}
+     {/* @ts-expect-error */}
      <LikeCount count={likes} title={slug} animation={false}></LikeCount>
     </div>
-    )
+  )
 }
  

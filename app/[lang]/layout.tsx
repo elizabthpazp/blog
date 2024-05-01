5cf6acedaccdea8ed1dbb0caee970f6879631e65
elizabthpazp <elizabethpazp695@gmail.com>
2024-04-30 21:40:57 -0600

    fix script

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -55,8 +55,6 @@ export default function RootLayout({
     <html lang={params.lang}>
       <head> 
        <meta name="google-site-verification" content="FKs04hcaiO7XyuBg9sogiZE3Hctm1YFcscQteeDZvIM" />
-       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7738434269106493"
-     crossorigin="anonymous"></script>
       </head>
       <body className="light:bg-[#4D4D4D] light:text-black dark:bg-[#17181C] dark:text-white">
         <Providers>
@@ -64,7 +62,8 @@ export default function RootLayout({
          
          <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="elizabethpH" data-description="Support me on Buy me a pizza!" data-message="Thank you so much for helping me keep creating content! 💜" data-color="rgb(124 58 237)" data-position="Right" data-x_margin="19" data-y_margin="15"></script>
          {/* <script defer src="https://app.embed.im/snow.js"></script> */}  
-       
+         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7738434269106493"
+     crossorigin="anonymous"></script>
          <GoogleAnalytics gaId="G-92CHRN38WP" />
         </Providers>
         <Analytics /> 
